608438a62072d62023bab015e00cc6c45f3bd8c1
kimoAth <kimo.daniel11@gmail.com>
2023-03-17 15:40:46 +0100

    noQ&E

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+*main - print alphabet lets except q and e 
+*
+*Return: return 0
+*/
+int main(void)
+{
+	int let;
+
+	for (let = 'a'; let <= 'z'; let++)
+	{
+		if (let == 'q' || let == 'e')
+		{
+			continue;
+		}
+		putchar(let);
+	}
+	putchar('\n');
+	return (0);
+}
