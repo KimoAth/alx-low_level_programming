415656e9c105e6be655e106e733902c00e9dfc02
kimoAth <kimo.daniel11@gmail.com>
2023-03-17 15:34:22 +0100

    printletter

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+*main - print alphabet letter
+*
+*Return: return 0
+*/
+int main(void)
+{
+	int let;
+	
+	for (let = 'a'; let <= 'z'; let++)
+	{
+		let = tolower(let);
+		putchar(let);
+	}
+	putchar('\n');
+	return (0);
+}
