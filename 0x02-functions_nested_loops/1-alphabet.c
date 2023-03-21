b81c7e383f4cbbbffd3cfb330dc525d169e9804a
kimoAth <kimo.daniel11@gmail.com>
2023-03-21 20:19:03 +0000

    alphabet printed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * print_alphabet - lowercase alphabet
-
+ *
 */
 void print_alphabet(void)
 {
 	int al;
-	for (al= 'a' ; al <= z ; al++)
+	for (al= 'a' ; al <= 'z' ; al++)
 	{
 		_putchar(al);
 	}	
