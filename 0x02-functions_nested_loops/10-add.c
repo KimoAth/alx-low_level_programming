fb48e465c6835173390cc7b0143de5de04811059
kimoAth <kimo.daniel11@gmail.com>
2023-03-21 23:03:22 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - function that adds two integers together
+ * @n: takes in an integer
+ * @m: takes in an integer
+ * Return: k the result
+ */
+int add(int n, int m)
+{
+	int k = n+m;
+	
+	return (k);
+
+
