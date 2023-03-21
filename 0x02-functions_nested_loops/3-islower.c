6c557c19d13d91d06d3a685d8b8139aba0a7ff0c
kimoAth <kimo.daniel11@gmail.com>
2023-03-21 21:40:23 +0000

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main"
+/**
+ * -islower - checks for lowercase
+ *
+ */
+int _islower(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 97  && c <= 122 )
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
