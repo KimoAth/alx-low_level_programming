20639294909de51e7e930602d7aa2e8ecffbf9e5
kimoAth <kimo.daniel11@gmail.com>
2023-03-21 22:16:24 +0000

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,14 +6,19 @@
  */
 int print_sign(int n)
 {
-	if (n>0)
+	if (n > 0)
 	{
-		printf("+");
+		_putchar("+");
 		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar("0");
+		return (0);
 	}
 	else
 	{
-		print("-");
-		return(0);
+		_putchar("-");
+		return (0);
 	}
 }
