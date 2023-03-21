0f84f7d9332c0a88adb29176f4a69a537d1fb905
kimoAth <kimo.daniel11@gmail.com>
2023-03-21 22:39:31 +0000

    main

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,12 +6,11 @@
  */
 int print_last_digit(int n)
 {
-       int ln = n % 10;
-
-       if (n < 0)
-       {
-	       ln = ln * -1;
-       }
-       _putchar(ln + '0');
-       return (ln);
+	int ln = n % 10;
+ 	if (n < 0)
+	{
+		ln = ln * -1;
+	}
+	_putchar(ln + '0');
+	return (ln);
 }
