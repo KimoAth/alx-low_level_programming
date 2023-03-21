3a0dcdda5854d4c68573e8aaed483cfdf770bc8d
kimoAth <kimo.daniel11@gmail.com>
2023-03-21 20:01:59 +0000

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /*
  * main - Entry point
- * @c: The character to print
+ * @l: The character to print
  * Return: Always 0 (Success)
 */
 int main(void)
@@ -9,11 +9,10 @@ int main(void)
 	char car[] = "_putchar";
 	int l;
 
-	for(l = 0; l < 8; l++)
+	for (l = 0; l < 8; l++)
 	{
 		_putchar(car[l]);
 	}
 	_putchar('\n');
-	
 	return (0);
 }
