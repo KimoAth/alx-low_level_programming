f3dd586273be22921aa9926ec69bf8f161e4403d
kimoAth <kimo.daniel11@gmail.com>
2023-03-21 20:36:46 +0000

    alphabet10tim

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabets 10 times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char al;
+	while (i <=10)
+	{	
+		for (al = 'a'; al <= 'z'; al++)
+			_putchar(al);
+		_putchar('\n');
+		i++;
+	}	
