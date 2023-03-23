56c525b5cecf84781b7ea00d40ce4a42a8caf8de
kimoAth <kimo.daniel11@gmail.com>
2023-03-23 23:24:31 +0000

    betty is annoying

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- *
- *
+ * more_numbers - prints 10 times numbers from 0 to 14
  *
  */
 void more_numbers(void)
@@ -12,9 +11,13 @@ void more_numbers(void)
 
 	while (t < 10)
 	{
-		for (i = 48; i < 57; i++)
+		for (i = 0; i <= 14; i++)
 		{
-	 		_putchar(i);
+			if (i > 9)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');	
 		t++;
