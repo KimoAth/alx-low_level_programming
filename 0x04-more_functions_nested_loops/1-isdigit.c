359f9bca46468235e809d5d1533dfc0a662c2462
kimoAth <kimo.daniel11@gmail.com>
2023-03-23 17:18:00 +0000

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,18 +2,14 @@
 
 /**
  * _isdigit - checks for digit 0 through 9
- * @c: the character that is gonna be checked
+ * @c: the character that be checked
  *
  * Return: 1 if c is digit 0 otherwise
  */
 int _isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
