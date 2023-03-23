eecf77f823addd50ef58b1571db950ef74f3856f
kimoAth <kimo.daniel11@gmail.com>
2023-03-23 16:58:34 +0000

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * _isupper : checks for uppercase character
  * @c: the character to be checked
+ * Return: 1 if c is uppercase otherwise 0
  */
 
 int _isupper(int c)
