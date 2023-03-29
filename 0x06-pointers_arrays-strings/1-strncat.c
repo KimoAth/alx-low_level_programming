c358aede05497a19106c52909fc2639b83a6d997
kimoAth <kimo.daniel11@gmail.com>
2023-03-29 18:06:05 +0000

    pointers

--- 0x06-pointers_arrays-strings/1-strncat.c
+++ 0x06-pointers_arrays-strings/1-strncat.c
@@ -1,27 +1,28 @@
 #include "main.h"
 /**
- * _strncat - concatenate strings, defining the size of second string.
- * @dest: string with concatenation
- * @src: string to be concatenated
- * @n: size of second string
- * Return: Always 0.
+ *_strncat - function that concatenates two strings. it will use
+ * at most n bytes from src. src does not need to be null terminated.
+ *@dest: pointer to destination string.
+ *@src: pointer to source string.
+ *@n: number of bytes to be concatenated.
+ *
+ *Return: pointer to destination string.
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int lengthD, lengthS;
+	int length, j;
+/* j is a counter for  n bytes of src to be concatenated */
+/* length = length of destination string */
 
-	lengthD = 0;
-	lengthS = 0;
-
-	while (*(dest + lengthD) != '\0')
-		lengthD++;
-
-	while (*(src + lengthS) != '\0' && lengthD < 97 && lengthS < n)
+	length = 0;
+	while (dest[length] != '\0')
 	{
-		*(dest + lengthD) = *(src + lengthS);
-		lengthD++;
-		lengthS++;
+		length++;
 	}
-	*(dest + lengthD) = '\0';
+	for (j = 0; j < n && src[j] != '\0'; j++, length++)
+	{
+		dest[length] = src[j];
+	}
+	dest[length] = '\0';
 	return (dest);
 }
--- 0x06-pointers_arrays-strings/3-strcmp.c
+++ 0x06-pointers_arrays-strings/3-strcmp.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
- * _strcmp - compare 2 strings.
- * @s1: string to be compared.
- * @s2: string for comparation.
- * Return: Difference.
+ *_strcmp - compares two strings.
+ *@s1: pointer to first string.
+ *@s2: pointer to second string.
+ *
+ *Return: value less than 0 if string is less than the other.
+ *value greater than 0 if string is greater than the other.
+ *0 if strings are equal.
  */
 int _strcmp(char *s1, char *s2)
 {
-	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+	int counter, cmpVal;
+
+	counter = 0;
+	while (s1[counter] == s2[counter] && s1[counter] != '\0')
 	{
-		s1++;
-		s2++;
+		counter++;
 	}
-	if (*s1 == *s2)
-	{
-		return (0);
-	}
-	else
-	{
-	return (*s1 - *s2);
-	}
+
+	cmpVal = s1[counter] - s2[counter];
+	return (cmpVal);
 }
