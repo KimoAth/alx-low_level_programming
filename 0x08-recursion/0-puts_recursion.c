3335914f56f952441672039d784e1c9fccdb0e47
kimoAth <kimo.daniel11@gmail.com>
2023-04-05 22:29:10 +0000

    string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,12 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * _puts_recursion(char *s): function that prints a string, followed by a new line.
- * @s: the string .
  *
- * Return: void.
+ *  @s: the string
+ *
+ * Return: void
 */
 
 void _puts_recursion(char *s){
