10c6dc8fa4d2f0a58431bc59e744a9db3012f72f
kimoAth <kimo.daniel11@gmail.com>
2023-04-17 21:43:32 +0000

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main(void) - prints the name of the file ,followed by a new line
+ *
+ * Return : 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
