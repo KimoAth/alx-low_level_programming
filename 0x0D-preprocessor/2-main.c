c967cbe784a850d6ef11482b7a68a0e196f8f2b9
kimoAth <kimo.daniel11@gmail.com>
2023-04-17 21:48:25 +0000

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main(void) - prints the name of the file it was compiled from
+ * main - prints the name of the file it was compiled from
  * ,followed by a new line
  *
  * Return : 0 (success)
