831b7138995dfc3d3231458462664d345ce7eaa5
kimoAth <kimo.daniel11@gmail.com>
2023-04-27 20:25:05 +0000

    linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * print_list -  function that prints all the elements of a list_t list.
+ * @count: counter
+ *
+ * Return: count
+ */
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
